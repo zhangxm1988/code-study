9585e53f88cce75c57147e26433246bf6822b213
zhangxm <zhxiaoming1988@163.com>
2019-06-05 15:50:54 +0800

    study red-black tree

---
+++ src/main/java/com/java/code/study/java/util/collection/TreeNodeTest.java
@@ -0,0 +1,22 @@
+package com.java.code.study.java.util.collection;
+
+/**
+ * @author zhangxm
+ * @description 树结构的学习
+ *
+ * @create 2019-06-05 15:10
+ */
+public class TreeNodeTest {
+
+  public static void main(String[] args) {
+    // study 红黑树 一种自平衡的二叉查找树，是一种高效的查找树
+    // 红黑树性质：
+    // 1、节点是红色或黑色。
+    // 2、根是黑色。
+    // 3、所有叶子都是黑色（叶子是NIL节点）。
+    // 4、每个红色节点必须有两个黑色的子节点。（从每个叶子到根的所有路径上不能有两个连续的红色节点。）
+    // 5、从任一节点到其每个叶子的所有简单路径都包含相同数目的黑色节点（简称黑高）。
+
+
+  }
+}
