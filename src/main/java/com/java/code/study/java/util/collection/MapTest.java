2787bb2cbe4c69fb5a2b863d6d322b7f7deae244
zhangxm <zhxiaoming1988@163.com>
2019-06-06 18:52:17 +0800

    study ConcurrentHashMap

--- src/main/java/com/java/code/study/java/util/collection/MapTest.java
+++ src/main/java/com/java/code/study/java/util/collection/MapTest.java
@@ -1,8 +1,10 @@
 package com.java.code.study.java.util.collection;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author zhangxm
@@ -65,6 +67,83 @@ public class MapTest {
       entry = iterator1.next();
       System.out.println("key=" + entry.getKey() + ",value=" + entry.getValue());
     }
+    System.out.println("study HashMap end");
+
+    // study LinkHashMap  类似HashMap + LinkList实现
+    // 以HashMap维护数据结构，内部维持了一个双向链表,可以保持顺序
+    // 存储数据你想保持进入的顺序与被取出的顺序一致的话，优先考虑LinkedMap
+    Map<String, String> linkHashMap = new LinkedHashMap<>();
+    linkHashMap.put(null, "empty");
+    linkHashMap.put("key", null);
+    Iterator<Map.Entry<String,String>> iterator2 = linkHashMap.entrySet().iterator();
+    Map.Entry<String, String> entry1;
+    while (iterator2.hasNext()) {
+      entry1 = iterator2.next();
+      System.out.println("key=" + entry1.getKey() + ",value=" + entry1.getValue());
+    }
+    System.out.println("study LinkHashMap end");
+
+    // study HashTable 数组 + 链表
+    // HashTable key和value都不允许为null
+    // HashTable 线程安全的（synchronized）
+    // 默认的初始化容量 11 （数组的大小） 最大容量 2的30次方
+    // 默认的填充因子（扩容）0.75  并且扩容必需是双倍的容量大小 + 1
+    // 迭代器与HashMap不同
+    Map<String, Integer> hashTable = new Hashtable<>();
+    hashTable.put("table1", 1);
+    hashTable.put("table2", 2);
+    for(String key1 : hashTable.keySet()) {
+      System.out.println("key=" + key1 + ",value=" + hashTable.get(key1));
+    }
+    System.out.println("study HashTable end");
+
+    // study Properties
+    Properties properties = new Properties();
+    ClassPathResource classPathResource = new ClassPathResource("application.properties");
+    try {
+      properties.load(classPathResource.getInputStream());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    System.out.println("study Properties:" + properties.getProperty("key"));
+
+    // study TreeMap  红黑树
+    Map<String, String> treeMap = new TreeMap<>(Comparator.comparing(Integer::parseInt));
+    treeMap.put("100", "value1");
+    treeMap.put("11", "value2");
+    treeMap.put("20", "value3");
+    treeMap.put("30", "value4");
+    Iterator<String> iterator3 = treeMap.keySet().iterator();
+    String key3;
+    while (iterator3.hasNext()) {
+      key3 = iterator3.next();
+      System.out.println("key=" + key3 + ",value=" + treeMap.get(key3));
+    }
+    System.out.println("study treeMap end");
+
+    // study weakHashMap  weakRefence弱引用
+    // 它的特殊之处在于 WeakHashMap 里的entry可能会被GC自动删除，即使程序员没有调用remove()或者clear()方法
+    // 适用场景：WeekHashMap 的这个特点特别适用于需要缓存的场景
+    Map<String, String> weakHashMap = new WeakHashMap<>();
+
+    // study ConcurrentHashMap
+    // 数据结构与HashMap数据结构相同
+    // key and value 不允许为null
+
+    Map<String, String> concurrentHashMap = new ConcurrentHashMap<>();
+    concurrentHashMap.put("key1", "value1");
+    concurrentHashMap.put("key2", "value2");
+    concurrentHashMap.put("key3", "value3");
+
+    for (String key4 : concurrentHashMap.keySet()) {
+      System.out.println("key=" + key4 + ",value=" + concurrentHashMap.get(key4));
+    }
+    System.out.println("study concurrentHashMap end");
+
+    int NCPU = Runtime.getRuntime().availableProcessors();
+    System.out.println("cpu available number:" + NCPU);
+    long freeMemory = Runtime.getRuntime().freeMemory();
+    System.out.println("free memory size:" + freeMemory);
 
   }
 
