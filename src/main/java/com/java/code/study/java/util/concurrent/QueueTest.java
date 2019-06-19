2e0e5a9fe77b547f77c1000e38eb23e131f41828
zhangxm <zhxiaoming1988@163.com>
2019-06-19 18:27:06 +0800

    start study queue

---
+++ src/main/java/com/java/code/study/java/util/concurrent/QueueTest.java
@@ -0,0 +1,15 @@
+package com.java.code.study.java.util.concurrent;
+
+/**
+ * @author zhangxm
+ * @description 队列学习
+ *
+ * @create 2019-06-19 18:25
+ */
+public class QueueTest {
+
+  public static void main(String[] args) {
+
+  }
+
+}
