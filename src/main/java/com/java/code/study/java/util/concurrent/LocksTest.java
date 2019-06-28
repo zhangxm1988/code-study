23049270565397063016074b37277dc5af2259e2
zhangxm <zhxiaoming1988@163.com>
2019-06-28 10:58:28 +0800

    study semaphore and lock finish

--- src/main/java/com/java/code/study/java/util/concurrent/LocksTest.java
+++ src/main/java/com/java/code/study/java/util/concurrent/LocksTest.java
@@ -1,6 +1,8 @@
 package com.java.code.study.java.util.concurrent;
 
+import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * @author zhangxm
@@ -10,10 +12,28 @@ import java.util.concurrent.locks.ReentrantLock;
  */
 public class LocksTest {
 
-  public static void main(String[] args) {
-
+  public static void main(String[] args) throws InterruptedException, BrokenBarrierException {
+    // 可重入锁（递归锁）
     ReentrantLock reenTrantLock;
 
+    // 读写锁
+    ReentrantReadWriteLock readWriteLock;
+
+    // CountDownLatch 闭锁
+    // 使用场景：CountDownLatch也叫闭锁，使得一(多)个主线程必须等待其他线程完成操作后再执行
+    CountDownLatch latch = new CountDownLatch(10);
+    latch.countDown();
+    latch.await();
+    latch.await(1000, TimeUnit.MILLISECONDS);
+
+    // CyclicBarrier  循环锁
+    // 可循环调用，它允许一组线程互相等待，直到到达某个公共屏障点 (common barrier point)。
+    // 所谓屏障点就是一组任务执行完毕的时刻
+    CyclicBarrier cyclicBarrier = new CyclicBarrier(10);
+    cyclicBarrier.await();
+
+    // Semaphore  信号量
+    Semaphore semaphore;
   }
 
 }
---
+++ src/main/java/com/java/code/study/java/util/concurrent/SemaphoreTest.java
@@ -0,0 +1,72 @@
+package com.java.code.study.java.util.concurrent;
+
+import org.apache.tomcat.util.threads.TaskThread;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author zhangxm
+ * @description 信号量
+ * @create 2019-06-28 10:18
+ */
+public class SemaphoreTest {
+
+  // Semaphore是一种在多线程环境下使用的设施，该设施负责协调各个线程，
+  // 以保证它们能够正确、合理的使用公共资源的设施，
+  // 也是操作系统中用于控制进程同步互斥的量。
+
+  // 信号量主要用于两个目的，一个是用于多个共享资源的互斥使用，另一个用于并发线程数的控制。
+
+  // 应用场景：1、我们以一个停车场运作为例来说明信号量的作用。
+  // 假设停车场只有三个车位，一开始三个车位都是空的。
+  // 这时如果同时来了三辆车，看门人允许其中它们进入进入，然后放下车拦。
+  // 以后来的车必须在入口等待，直到停车场中有车辆离开。
+  // 这时，如果有一辆车离开停车场，看门人得知后，打开车拦，放入一辆，
+  // 如果又离开一辆，则又可以放入一辆，如此往复。
+  // 2、并发线程数控制，比例接口允许最大的并发数。
+
+  private Semaphore semaphore = new Semaphore(3);
+  private Random random = new Random();
+
+  class TaskThread implements Runnable {
+    private String id;
+
+    public TaskThread(String id) {
+      this.id = id;
+    }
+
+    @Override
+    public void run() {
+      try {
+        semaphore.acquire();
+        System.out.println("Thread " + id + " is working");
+        Thread.sleep(random.nextInt(1000));
+        semaphore.release();
+
+        System.out.println("Thread " + id + " is over");
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    ExecutorService executorService = Executors.newCachedThreadPool();
+
+    SemaphoreTest semaphoreTest = new SemaphoreTest();
+
+    executorService.submit(semaphoreTest.new TaskThread("a"));
+    executorService.submit(semaphoreTest.new TaskThread("b"));
+    executorService.submit(semaphoreTest.new TaskThread("c"));
+    executorService.submit(semaphoreTest.new TaskThread("d"));
+    executorService.submit(semaphoreTest.new TaskThread("e"));
+    executorService.submit(semaphoreTest.new TaskThread("f"));
+    executorService.submit(semaphoreTest.new TaskThread("g"));
+
+    executorService.shutdown();
+  }
+
+}
