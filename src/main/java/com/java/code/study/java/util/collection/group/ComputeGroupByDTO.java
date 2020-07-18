6cadaae1f431748fa789c9743bf2ce65c790c9c4
zhangxiaoming <xiaoming.zhang@zhiyoubao.com>
2020-07-18 13:37:15 +0800

    study java use stream group statistic

---
+++ src/main/java/com/java/code/study/java/util/collection/group/ComputeGroupByDTO.java
@@ -0,0 +1,29 @@
+package com.java.code.study.java.util.collection.group;
+
+import lombok.Data;
+
+/**
+ * @author xiaoming.zhang@zhiyoubao.com
+ * @description 用于统计的值
+ *
+ * @date 2020/7/18 13:29
+ */
+@Data
+public class ComputeGroupByDTO {
+
+    /**
+     * 总条数
+     */
+    private long totalCount;
+
+    /**
+     * 总的工作小时数
+     */
+    private Double dueHourSum;
+
+    /**
+     * 真实的小时数
+     */
+    private Double realHourSum;
+
+}
